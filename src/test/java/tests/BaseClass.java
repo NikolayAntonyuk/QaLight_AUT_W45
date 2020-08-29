9b37090b9dd6668ecd1c384fdc689d5aa7b1d625
Crushpowerx <asovin.e.v@gmail.com>
2020-08-29 12:44:26 +0300

    Update TestNG.xml file

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -2,12 +2,26 @@ package tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
     Calculator calculator = new Calculator();
 
+    @Parameters({"browserName", "country"})
     @BeforeMethod
-    public void prepareProject() {
+    public void prepareProject(@Optional String browserName, @Optional String country) {
+        System.out.println("Environments:");
+        if (browserName.equals("Chrome")) {
+            System.out.println("Browser name: " + browserName);
+        } else {
+            System.out.println("Browser name: " + browserName + " not found");
+        }
+        if (country.equals("SE")) {
+            System.out.println("Country: " + country);
+        } else {
+            System.out.println("Country: " + country + " not found");
+        }
         System.out.println("Before method");
     }
 
