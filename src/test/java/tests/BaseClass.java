59bdf3330c2e3ed35abb503b0bbb13fb19eb315f
anastasiiayermolenko <agnipova24@gmail.com>
2020-08-29 12:52:02 +0300

    first commit

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -2,13 +2,19 @@ package tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
     Calculator calculator = new Calculator();
 
+    @Parameters({"browserName", "country"})
     @BeforeMethod
-    public void prepareProject() {
-        System.out.println("Before method");
+    public void prepareProject(@Optional String browserName, @Optional String country) {
+        System.out.println("Environments:");
+        if (browserName.equals("Chrome")) {
+            System.out.println("Browser name: " + browserName);
+        }
     }
 
     @AfterMethod
