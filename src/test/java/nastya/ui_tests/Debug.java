13c355bff39866f7cf96927d95aafe414342844f
anastasiiayermolenko <agnipova24@gmail.com>
2020-09-05 12:06:26 +0300

    ui_test1

---
+++ src/test/java/nastya/ui_tests/Debug.java
@@ -0,0 +1,30 @@
+package nastya.ui_tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Debug {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+         String baseUrl = "https://ru.wikipedia.org/";
+         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+         driver = new ChromeDriver();
+         driver.get(baseUrl);
+         driver.manage().window().maximize();
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("start");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+}
