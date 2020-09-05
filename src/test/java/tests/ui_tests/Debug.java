e9539fed8b161cdf0c2954d84ea313846cf9bd2c
Crushpowerx <asovin.e.v@gmail.com>
2020-09-05 13:04:21 +0300

    upd

--- src/test/java/tests/ui_tests/Debug.java
+++ src/test/java/tests/ui_tests/Debug.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 public class Debug {
     WebDriver driver;
